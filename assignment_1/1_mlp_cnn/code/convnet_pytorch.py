e289a2253ffad54b6182987f7f3e29dfed2230e4
Eui Yeon Jang <euiyeonjang@gmail.com>
2020-11-15 13:51:30 +0100

    clean up code

--- assignment_1/1_mlp_cnn/code/convnet_pytorch.py
+++ assignment_1/1_mlp_cnn/code/convnet_pytorch.py
@@ -47,53 +47,65 @@ class ConvNet(nn.Module):
         # PUT YOUR CODE HERE  #
         #######################
         super().__init__()
+
         channels = [n_channels, 64, 128, 256, 512]
 
-        self.MaxPool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        MaxPool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         ReLU = nn.ReLU()
 
-        self.conv0 = nn.Conv2d(channels[0], channels[1], kernel_size=3, padding=1)
+        conv0 = nn.Conv2d(channels[0], channels[1], kernel_size=3, padding=1)
 
-        self.preact1 = PreActResNet(channels[1])
+        preact1 = PreActResNet(channels[1])
 
-        self.conv1 = nn.Sequential(
+        conv1 = nn.Sequential(
             nn.Conv2d(channels[1], channels[2], kernel_size=1),
-            self.MaxPool
+            MaxPool
         )
 
-        self.preact2a = PreActResNet(channels[2])
+        preact2a = PreActResNet(channels[2])
         
-        self.preact2b = PreActResNet(channels[2])
+        preact2b = PreActResNet(channels[2])
 
-        self.conv2 = nn.Sequential(
+        conv2 = nn.Sequential(
             nn.Conv2d(channels[2], channels[3], kernel_size=1),
-            self.MaxPool
+            MaxPool
         )
 
-        self.preact3a = PreActResNet(channels[3])
+        preact3a = PreActResNet(channels[3])
         
-        self.preact3b = PreActResNet(channels[3])
+        preact3b = PreActResNet(channels[3])
 
-        self.conv3 = nn.Sequential(
+        conv3 = nn.Sequential(
             nn.Conv2d(channels[3], channels[4], kernel_size=1),
-            self.MaxPool
+            MaxPool
         )
 
-        self.preact4a = PreActResNet(channels[4])
+        preact4a = PreActResNet(channels[4])
         
-        self.preact4b = PreActResNet(channels[4])
+        preact4b = PreActResNet(channels[4])
 
-        self.preact5a = PreActResNet(channels[4])
+        preact5a = PreActResNet(channels[4])
         
-        self.preact5b = PreActResNet(channels[4])
+        preact5b = PreActResNet(channels[4])
 
-        self.last = nn.Sequential(
-            self.MaxPool,
+        last = nn.Sequential(
+            MaxPool,
             nn.BatchNorm2d(channels[4]),
             ReLU,
             nn.Flatten(),
             nn.Linear(channels[4], n_classes)
         )
+
+        self.list_modules = [
+            conv0,
+            preact1, conv1,
+            preact2a, preact2b, conv2,
+            preact3a, preact3b, conv3,
+            preact4a, preact4b, MaxPool,
+            preact5a, preact5b, last
+        ]
+
+        self.list_modules = nn.ModuleList(self.list_modules)
 
         ########################
         # END OF YOUR CODE    #
@@ -116,21 +128,10 @@ class ConvNet(nn.Module):
         ########################
         # PUT YOUR CODE HERE  #
         #######################
-        x = self.conv0(x)
-        x = self.preact1(x)
-        x = self.conv1(x)
-        x = self.preact2a(x)
-        x = self.preact2b(x)
-        x = self.conv2(x)
-        x = self.preact3a(x)
-        x = self.preact3b(x)
-        x = self.conv3(x)
-        x = self.preact4a(x)
-        x = self.preact4b(x)
-        x = self.MaxPool(x)
-        x = self.preact5a(x)
-        x = self.preact5b(x)
-        out = self.last(x)
+        for m in self.list_modules:
+            x = m(x)
+
+        out = x
         ########################
         # END OF YOUR CODE    #
         #######################
