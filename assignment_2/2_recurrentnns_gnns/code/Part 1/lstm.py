1cdb400ca0960f13edbc8b070d7cb2d7a4f13ef1
Eui Yeon Jang <euiyeonjang@gmail.com>
2020-11-21 19:25:15 +0100

    remove device

--- assignment_2/2_recurrentnns_gnns/code/Part 1/lstm.py
+++ assignment_2/2_recurrentnns_gnns/code/Part 1/lstm.py
@@ -16,24 +16,24 @@ class LSTMCell(nn.Module):
         super().__init__()
 
         # Weights
-        self.W_gx = nn.Parameter(torch.empty(hidden_dim, input_dim)).to(device) 
-        self.W_ix = nn.Parameter(torch.empty(hidden_dim, input_dim)).to(device) 
-        self.W_fx = nn.Parameter(torch.empty(hidden_dim, input_dim)).to(device) 
-        self.W_ox = nn.Parameter(torch.empty(hidden_dim, input_dim)).to(device) 
+        self.W_gx = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+        self.W_ix = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+        self.W_fx = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+        self.W_ox = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
 
-        self.W_gh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)).to(device) 
-        self.W_ih = nn.Parameter(torch.empty(hidden_dim, hidden_dim)).to(device) 
-        self.W_fh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)).to(device) 
-        self.W_oh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)).to(device) 
+        self.W_gh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+        self.W_ih = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+        self.W_fh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+        self.W_oh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
 
         # biases
-        self.b_g = nn.Parameter(torch.zeros(hidden_dim, 1)).to(device)
-        self.b_i = nn.Parameter(torch.zeros(hidden_dim, 1)).to(device)
-        self.b_f = nn.Parameter(torch.zeros(hidden_dim, 1)).to(device)
-        self.b_o = nn.Parameter(torch.zeros(hidden_dim, 1)).to(device)
+        self.b_g = nn.Parameter(torch.zeros(hidden_dim, 1))
+        self.b_i = nn.Parameter(torch.zeros(hidden_dim, 1))
+        self.b_f = nn.Parameter(torch.zeros(hidden_dim, 1))
+        self.b_o = nn.Parameter(torch.zeros(hidden_dim, 1))
 
-        self.W_ph = nn.Parameter(torch.empty(num_classes, hidden_dim)).to(device)
-        self.b_p = nn.Parameter(torch.zeros(1, num_classes)).to(device)
+        self.W_ph = nn.Parameter(torch.empty(num_classes, hidden_dim))
+        self.b_p = nn.Parameter(torch.zeros(1, num_classes))
 
         # non-linearity
         self.tanh = nn.Tanh()
@@ -79,9 +79,34 @@ class LSTM(nn.Module):
         self.batch_size = batch_size
         self.device = device
         embedding_size = 10 #int(hidden_dim/2)
-        self.embedding = nn.Embedding(input_dim, embedding_size).to(device)
+        self.embedding = nn.Embedding(input_dim, embedding_size)
 
-        self.cell = LSTMCell(embedding_size, hidden_dim, num_classes, device).to(device)
+        # self.cell = LSTMCell(embedding_size, hidden_dim, num_classes, device)
+        # Weights
+        self.W_gx = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+        self.W_ix = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+        self.W_fx = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+        self.W_ox = nn.Parameter(torch.empty(hidden_dim, input_dim)) 
+
+        self.W_gh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+        self.W_ih = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+        self.W_fh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+        self.W_oh = nn.Parameter(torch.empty(hidden_dim, hidden_dim)) 
+
+        # biases
+        self.b_g = nn.Parameter(torch.zeros(hidden_dim, 1))
+        self.b_i = nn.Parameter(torch.zeros(hidden_dim, 1))
+        self.b_f = nn.Parameter(torch.zeros(hidden_dim, 1))
+        self.b_o = nn.Parameter(torch.zeros(hidden_dim, 1))
+
+        self.W_ph = nn.Parameter(torch.empty(num_classes, hidden_dim))
+        self.b_p = nn.Parameter(torch.zeros(1, num_classes))
+
+        # non-linearity
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=-1)
+
 
         self.init_weights()
         ########################
@@ -90,17 +115,13 @@ class LSTM(nn.Module):
 
     def init_weights(self):
         for name, param in self.named_parameters():
-            if name.startswith("cell.W_"):
+            if name.startswith("W_"):
                 print(name)
                 nn.init.kaiming_normal_(param, nonlinearity="linear")
 
     def init_states(self): 
-        self.c = torch.zeros(self.batch_size, self.hidden_dim).to(self.device)
-        self.h = torch.zeros(self.batch_size, self.hidden_dim).to(self.device)
-
-        # nn.init.kaiming_normal_(self.c, nonlinearity="linear") #, mode='fan_in', nonlinearity='relu')
-        # nn.init.kaiming_normal_(self.h, nonlinearity="linear") #, mode='fan_in', nonlinearity='relu')
-
+        self.c = torch.zeros(self.batch_size, self.hidden_dim)
+        self.h = torch.zeros(self.batch_size, self.hidden_dim)
 
     def forward(self, x):
         ########################
@@ -113,7 +134,24 @@ class LSTM(nn.Module):
 
         for t in range(self.seq_length):
             # y, self.c, self.h = self.cell(embed_x[:,:,t], self.c, self.h)
-            y, self.c, self.h = self.cell(embed_x[:,t], self.c, self.h)
+            # y, self.c, self.h = self.cell(embed_x[:,t], self.c, self.h)
+            # Eq 4
+            x = embed_x[:, t]
+            g = self.tanh(x@self.W_gx.T + self.h@self.W_gh.T + self.b_g)
+            # Eq 5
+            i = self.sigmoid(x@self.W_ix.T + self.h@self.W_ih.T + self.b_i)
+            # Eq 6
+            f = self.sigmoid(x@self.W_fx.T + self.h@self.W_fh.T + self.b_f)
+            # Eq 7
+            o = self.sigmoid(x@self.W_ox.T + self.h@self.W_oh.T + self.b_o)
+
+            # Eq 8
+            self.c = g * i + self.c * f
+            # Eq 9
+            self.h = self.tanh(self.c) * o
+
+            # Eq 10
+            y = self.h@self.W_ph.T + self.b_p
 
         return y
         ########################
